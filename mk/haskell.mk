cb8d7e8a6e3fcb4c33a6076e7e5126e57d55cde0
Rudy Matela <rudy@matela.com.br>
2024-02-08 14:22:28 +0100

    use avoid-reinstalls

--- mk/haskell.mk
+++ mk/haskell.mk
@@ -92,7 +92,7 @@ depend:
 install-dependencies:
 	if [ -n "$(INSTALL_DEPS)" ]; then \
 		cabal update && \
-		$(CABAL_INSTALL) $(INSTALL_DEPS); \
+		$(CABAL_INSTALL) $(INSTALL_DEPS) --avoid-reinstalls; \
 	fi
 
 # haddock rules
